2c6f6462fd346f16ed7b7634c5098332434fcc04
Yashi720 <154510460+Yashi720@users.noreply.github.com>
2026-04-22 22:55:51 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        int a = sc.nextInt();
+        System.out.println("Enter second number:");
+        int b = sc.nextInt();
+        System.out.println("Enter the operator:");
+        char op = sc.next().charAt(0);
+
+        switch (op) {
+            case '+':
+                System.out.println(a + b);
+                break;
+            case '-':
+                System.out.println(a - b);
+                break;
+            case '*':
+                System.out.println(a * b);
+                break;
+            case '/':
+                System.out.println(a / b);
+                break;
+            case '%':
+                System.out.println(a % b);
+                break;
+            default:
+                System.out.println("Invalid operator");
+        }
+        sc.close();
+    }
+}
